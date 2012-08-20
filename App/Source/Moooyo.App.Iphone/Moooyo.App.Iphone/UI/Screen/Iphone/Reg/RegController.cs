3fce23541e577169eaa89c2fdf8c1d09b7d36ec8
John0122 <527087796@qq.com>
2012-08-20 17:21:05 +0800

    Revert "aa"
    This reverts commit 415274382045a22ea30211a57b2b2d7b4f9705ea.

--- App/Source/Moooyo.App.Iphone/Moooyo.App.Iphone/UI/Screen/Iphone/Reg/RegController.cs
+++ App/Source/Moooyo.App.Iphone/Moooyo.App.Iphone/UI/Screen/Iphone/Reg/RegController.cs
@@ -15,10 +15,13 @@ namespace Moooyo.App.Iphone
 	public partial class RegController : UIViewController
 	{
 
-		private UIImage AGREEMENTNO = new UIImage("UI/Image/Agreement/AgreementNo.png");
-		private UIImage AGREEMENTYES = new UIImage("UI/Image/Agreement/AgreementYes.png");
-		private UIImage SUBMITIMAGEOUT = new UIImage("UI/Image/Reg/SubmitImageOut.png");
-		private UIImage SUBMITIMAGEIN = new UIImage("UI/Image/Reg/SubmitImageIn.png");
+		private UIImage NABACKIMAGE = new UIImage("UI/Image/Comm/NavigationBackroundImage.png");
+		private UIImage NABARBACKIMAGE = new UIImage("UI/Image/Comm/BackButtonImage.png");
+		private UIImage NABARCLOSEIMAGE = new UIImage("UI/Image/Comm/CloseButtonImage.png");
+		private UIImage AGREEMENTNO = new UIImage("UI/Image/Reg/AgreementNo.png");
+		private UIImage AGREEMENTYES = new UIImage("UI/Image/Reg/AgreementYes.png");
+		private UIImage SUBMITBACKIMAGE = new UIImage("UI/Image/Reg/SubmitBackImage.png");
+		private UIImage SUBMITCLICKBACKIMAGE = new UIImage("UI/Image/Reg/SubmitClickBackImage.png");
 
 		private bool AGREEMENTCHECKED = true;
 
@@ -30,6 +33,33 @@ namespace Moooyo.App.Iphone
 		
 		public override void ViewDidLoad ()
 		{
+			this.Title = "加入米柚";
+			this.View.BackgroundColor = UIColor.FromRGB(240, 240, 240);
+
+			UIImageView imageView = new UIImageView(NABACKIMAGE);
+
+			UILabel titleLable = new UILabel(new RectangleF(110, 0, 100, 44));
+			titleLable.Text = "加入米柚";
+			titleLable.BackgroundColor = UIColor.Clear;
+			titleLable.TextAlignment = UITextAlignment.Center;
+			titleLable.TextColor = UIColor.FromRGB(255, 255, 255);
+			titleLable.Font = UIFont.FromName(FONTFAMILY, 16f);
+
+			UIButton backButton = new UIButton(new RectangleF(0, 0, 44, 44));
+			backButton.SetBackgroundImage(NABARBACKIMAGE, UIControlState.Normal);
+			backButton.BackgroundColor = UIColor.Clear;
+
+			UIButton closeButton = new UIButton(new RectangleF(276, 0, 44, 44));
+			closeButton.SetBackgroundImage(NABARCLOSEIMAGE, UIControlState.Normal);
+			closeButton.BackgroundColor = UIColor.Clear;
+
+			NavigationController.NavigationBar.AddSubview(imageView);
+			NavigationController.NavigationBar.Add(titleLable);
+			NavigationController.NavigationBar.Add(backButton);
+			NavigationController.NavigationBar.Add(closeButton);
+
+			Console.WriteLine(EmailText.Font.FamilyName);
+
 			LoadTextEventsAndSth();
 
 			CheckButton.SetImage(AGREEMENTYES, UIControlState.Normal);
@@ -47,38 +77,29 @@ namespace Moooyo.App.Iphone
 				}
 			};
 
-			ShowAgreement.TouchUpInside += (sender, e) => 
-			{
-				UsageAgreement.ShowAgreement(this);
-			};
-
-			SubmitButton.SetImage(SUBMITIMAGEOUT, UIControlState.Normal);
-			UILabel submitTitle = new UILabel(new RectangleF(95, 0, 100, 45));
+			SubmitButton.SetImage(SUBMITBACKIMAGE, UIControlState.Normal);
+			UILabel submitTitle = new UILabel(new RectangleF(90, 0, 100, 40));
 			submitTitle.Text = "注册";
 			submitTitle.TextColor = UIColor.FromRGB(255, 255, 255);
 			submitTitle.TextAlignment = UITextAlignment.Center;
-			submitTitle.Font = UIFont.FromName(FONTFAMILY, 18);
+			submitTitle.Font = UIFont.FromName(FONTFAMILY, 16);
 			submitTitle.BackgroundColor = UIColor.Clear;
 			SubmitButton.Add(submitTitle);
 			SubmitButton.TouchUpInside += (sender, e) => 
 			{
-				SubmitButton.SetImage(SUBMITIMAGEOUT, UIControlState.Normal);
+				SubmitButton.SetImage(SUBMITBACKIMAGE, UIControlState.Normal);
 				if(Submit())
 				{
-					CustomAlert.ShowCustomAlert(CustomAlertType.Wait, "正在提交注册信息", this);
-					Reg2Controller reg2 = new Reg2Controller();
-					reg2.NavigationItem.SetHidesBackButton(true, true);
-					NavigationController.PushViewController(reg2, true);
-					CustomAlert.CloseCustomAlert();
+
 				}
 			};
 			SubmitButton.TouchDown += (sender, e) => 
 			{
-				SubmitButton.SetImage(SUBMITIMAGEIN, UIControlState.Normal);
+				SubmitButton.SetImage(SUBMITCLICKBACKIMAGE, UIControlState.Normal);
 			};
 			SubmitButton.TouchDragInside += (sender, e) => 
 			{
-				SubmitButton.SetImage(SUBMITIMAGEOUT, UIControlState.Normal);
+				SubmitButton.SetImage(SUBMITBACKIMAGE, UIControlState.Normal);
 			};
 
 			base.ViewDidLoad ();
@@ -86,10 +107,14 @@ namespace Moooyo.App.Iphone
 
 		public void LoadTextEventsAndSth ()
 		{
-			EmailText.Text = "john@gmail.com";
-			PasswordText.Text = "123456";
-			ConfirmText.Text = "123456";
-			NickNameText.Text = "John";
+			EmailView.BackgroundColor = PasswordView.BackgroundColor = ConfirmView.BackgroundColor = NickNameView.BackgroundColor = UIColor.Clear;
+
+			EmailView.Image = PasswordView.Image = ConfirmView.Image = NickNameView.Image = TextEventsAndSth.OUTTEXTBORDERIMAGE;
+
+			EmailClose.SetImage(TextEventsAndSth.TEXTCLOSEIMAGE, UIControlState.Normal);
+			PasswordClose.SetImage(TextEventsAndSth.TEXTCLOSEIMAGE, UIControlState.Normal);
+			ConfirmClose.SetImage(TextEventsAndSth.TEXTCLOSEIMAGE, UIControlState.Normal);
+			NickNameClose.SetImage(TextEventsAndSth.TEXTCLOSEIMAGE, UIControlState.Normal);
 
 			List<UIImageView> viewList = new List<UIImageView>{ EmailView, PasswordView, ConfirmView, NickNameView };
 			List<UITextField> textList = new List<UITextField>{ EmailText, PasswordText, ConfirmText, NickNameText };
@@ -109,27 +134,27 @@ namespace Moooyo.App.Iphone
 			Boolean mailmatch = Regex.IsMatch (mail, @"^([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$");
 			Boolean passwordmatch = Regex.IsMatch (password, @"^[A-Za-z0-9]+$");
 			if (!mailmatch) {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "请输入正确的邮箱地址", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "请输入正确的邮箱地址", this);
 				return false;
 			}
 			if (password.Trim ().Length < 6) {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "您输入的密码太短了", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "您输入的密码太短了", this);
 				return false;
 			}
 			if (!passwordmatch) {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "请输入正确的密码", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "请输入正确的密码", this);
 				return false;
 			}
 			if (confirm.Trim () != password.Trim ()) {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "两次输入的密码不一致", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "两次输入的密码不一致", this);
 				return false;
 			}
 			if (nickname.Trim () == "") {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "请输入您的昵称", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "请输入您的昵称", this);
 				return false;
 			}
 			if (!AGREEMENTCHECKED) {
-				CustomAlert.ShowCustomAlert (CustomAlertType.Warning, "您必须同意使用协议才能注册", this);
+				CustomAlert.ShowCustomAlert (CustomAlertType.Error, "您必须同意使用协议才能注册", this);
 				return false;
 			}
 //			Accounts submitController = new Accounts ();
@@ -141,12 +166,6 @@ namespace Moooyo.App.Iphone
 //				return false;
 //			}
 			return true;
-		}
-
-		public override void ViewWillAppear (bool animated)
-		{
-			LoadNavigation.LoadNa(this, "加入米柚");
-			base.ViewWillAppear (animated);
 		}
 
 		public override void ViewDidUnload ()
