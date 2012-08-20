ad996caafaeaa3d6e30bca8c8278b9e63182e605
John0122 <527087796@qq.com>
2012-08-20 17:21:05 +0800

    Revert "Revert "Reg2""
    This reverts commit 6448754d07c90c7f8452532eb7b340168dcd8250.

--- App/Source/Moooyo.App.Iphone/Moooyo.App.Iphone/UI/Controls/Iphone/CustomAlert.cs
+++ App/Source/Moooyo.App.Iphone/Moooyo.App.Iphone/UI/Controls/Iphone/CustomAlert.cs
@@ -10,50 +10,57 @@ namespace Moooyo.App.Iphone
 {
 	public class CustomAlert
 	{
-		public static UIImage ALERTMESSAGEBACK = new UIImage("UI/Image/Comm/AlertMessageBack.png");
-		public static UIImage ALERTMESSAGEERROR = new UIImage("UI/Image/Comm/AlertMessageError.png");
+		private static UIImage ALERTMESSAGEBACK = new UIImage("UI/Image/CustomAlert/AlertMessageBack.png");
+		private static UIImage ALERTMESSAGEERROR = new UIImage("UI/Image/CustomAlert/AlertMessageError.png");
+		private static UIImage ALERTMESSAGEWARNING = new UIImage("UI/Image/CustomAlert/AlertMessageWarning.png");
+		private static UIImage ALERTMESSAGEWAIT = new UIImage("UI/Image/CustomAlert/AlertMessageWait.png");
 
-		public static string FONTFAMILY = "STHeitiSC-Medium";
+		private static string FONTFAMILY = "STHeitiSC-Medium";
+
+		private static UIView maskView;
+		private static UIView alertButton;
 
 		public static void ShowCustomAlert (CustomAlertType type, string message, UIViewController mainView)
 		{
 			float width = mainView.View.Frame.Width;
 			float height = mainView.View.Frame.Height + mainView.NavigationController.NavigationBar.Frame.Height + 20;
-			List<UIView> alertViews = CustomAlerts (width, height, type, message);
-			foreach (UIView view in alertViews) 
-			{
-				mainView.NavigationController.View.Add(view);
-			}
+			maskView = Mask.GetMask(width, height);
+			alertButton = GetCustomButton(width, height, type, message);
+			mainView.NavigationController.View.Add(maskView);
+			mainView.NavigationController.View.Add(alertButton);
 		}
 
-		public static List<UIView> CustomAlerts (float viewWidth, float viewHeight, CustomAlertType type, string message)
+		public static void CloseCustomAlert ()
 		{
-			List<UIView> uiviews = new List<UIView>();
+			maskView.RemoveFromSuperview();
+			alertButton.RemoveFromSuperview();
+		}
 
-			UIView alertBackView = new UIView();
-			alertBackView.Alpha = 0.5f;
-			alertBackView.Frame = new RectangleF(0, 0, viewWidth, viewHeight);
-			alertBackView.BackgroundColor = UIColor.FromRGB(0, 0, 0);
-
+		public static UIView GetCustomButton (float viewWidth, float viewHeight, CustomAlertType type, string message)
+		{
 			float width = 300f;
 			float height = 60f;
 			float x = (viewWidth - width) / 2;
 			float y = (viewHeight - height * 1.5f) / 2;
-			UIButton alertButton = new UIButton ();
-			alertButton.Frame = new RectangleF (x, y, width, height);
+
+			UIView buttonView = new UIView(new RectangleF (x, y, width, height));
+			buttonView.BackgroundColor = UIColor.Clear;
+
+			UIButton alertButton = new UIButton (new RectangleF(0, 0, width, height));
 			alertButton.BackgroundColor = UIColor.Clear;
 			alertButton.SetImage(ALERTMESSAGEBACK, UIControlState.Normal);
 			alertButton.AdjustsImageWhenHighlighted = false;
 			alertButton.TouchUpInside += (sender, e) => 
 			{
-				alertBackView.RemoveFromSuperview();
-				alertButton.RemoveFromSuperview();
+				CloseCustomAlert();
 			};
 			UIImageView imageView = new UIImageView(new RectangleF(20, 20, 20, 20));
 			switch (type) 
 			{
 				case CustomAlertType.Error: imageView.Image = ALERTMESSAGEERROR; break;
 				case CustomAlertType.OK:break;
+				case CustomAlertType.Warning:imageView.Image = ALERTMESSAGEWARNING; break;
+				case CustomAlertType.Wait:imageView.Image = ALERTMESSAGEWAIT; break;
 			}
 			UILabel messageLable = new UILabel();
 			messageLable.Text = message;
@@ -62,17 +69,21 @@ namespace Moooyo.App.Iphone
 			messageLable.Frame = new RectangleF(50, 10, width - 80, height - 20);
 			messageLable.TextAlignment = UITextAlignment.Left;
 			messageLable.BackgroundColor = UIColor.Clear;
+
 			alertButton.Add(imageView);
 			alertButton.Add(messageLable);
-			uiviews.Add(alertBackView);
-			uiviews.Add(alertButton);
-			return uiviews;
+
+			buttonView.Add(alertButton);
+
+			return buttonView;
 		}
 	}
 	public enum CustomAlertType
 	{
 		OK = 0,
-		Error = 1
+		Error = 1,
+		Warning = 2,
+		Wait = 3
 	}
 }
 
